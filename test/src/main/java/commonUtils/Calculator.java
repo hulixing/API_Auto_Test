6a58d154ffeddf13d16d39e1e8834f3a9ea76e6a
hulixing <37899968+hulixing@users.noreply.github.com>
2018-06-07 20:30:29 +0800

    learning

---
+++ test/src/main/java/commonUtils/Calculator.java
@@ -0,0 +1,17 @@
+package commonUtils;
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int a = 0 ;
+		int b = 0 ;
+		
+		Add(a,b);
+	}
+
+	private static void Add(int a, int b) {
+		System.out.println(a+b);
+		
+	}
+	
+}
